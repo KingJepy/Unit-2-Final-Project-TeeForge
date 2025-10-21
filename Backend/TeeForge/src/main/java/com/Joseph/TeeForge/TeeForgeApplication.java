dea5f8f82047897ace895d7825d64003f6175bbb
KingJepy <jmoslander15@gmail.com>
2025-10-20 21:01:13 -0500

    initial commit after spring initializr

---
+++ Backend/TeeForge/src/main/java/com/Joseph/TeeForge/TeeForgeApplication.java
@@ -0,0 +1,13 @@
+package com.Joseph.TeeForge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TeeForgeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TeeForgeApplication.class, args);
+	}
+
+}
---
+++ Backend/TeeForge/src/test/java/com/Joseph/TeeForge/TeeForgeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Joseph.TeeForge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TeeForgeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
