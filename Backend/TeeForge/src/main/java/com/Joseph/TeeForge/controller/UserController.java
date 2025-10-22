7d3353d9e005937f7c559bc426646b8276ed0339
KingJepy <jmoslander15@gmail.com>
2025-10-22 17:42:58 -0500

    completed my user controller with crud ops. may update later

--- Backend/TeeForge/src/main/java/com/Joseph/TeeForge/controller/UserController.java
+++ Backend/TeeForge/src/main/java/com/Joseph/TeeForge/controller/UserController.java
@@ -1,4 +1,54 @@
 package com.Joseph.TeeForge.controller;
 
+
+import com.Joseph.TeeForge.model.User;
+import com.Joseph.TeeForge.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/api/users")
+@CrossOrigin(origins = "http://localhost:3000")
 public class UserController {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    // get all users
+    @GetMapping
+    public List<User> getUserById() {
+        return userRepository.findAll();
+    }
+
+    //get single user by id
+    @GetMapping("/{id}")
+    public Optional<User> getUserById(@PathVariable int id) {
+        return userRepository.findById(id);
+    }
+
+    // update a user
+    @PutMapping("/{id}")
+    public User updateUser(@PathVariable Integer id, @RequestBody User updatedUser) {
+        return userRepository.findById(id)
+                .map(user -> {
+                    user.setUsername(updatedUser.getUsername());
+                    user.setEmail(updatedUser.getEmail());
+                    user.setPasswordHash(updatedUser.getPasswordHash());
+                    return userRepository.save(user);
+                })
+                .orElseGet(() -> {
+                    updatedUser.setUserId(id);
+                    return userRepository.save(updatedUser);
+                });
+    }
+
+    // delete a user
+    @DeleteMapping("/{id}")
+    public void deleteUser(@PathVariable Integer id) {
+        userRepository.deleteById(id);
+    }
+
 }
