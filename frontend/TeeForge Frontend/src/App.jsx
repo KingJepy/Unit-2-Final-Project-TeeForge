055227ae0855673e036ad92fc98f044fcd6f6c2c
KingJepy <jmoslander15@gmail.com>
2025-10-27 18:04:56 -0500

    started new react project inside the frontend folder. wll start copying code over next

---
+++ frontend/TeeForge Frontend/src/App.jsx
@@ -0,0 +1,32 @@
+
+import './App.css'
+import AboutPage from './components/AboutPage';
+import Footer from './components/Footer';
+import Header from './components/Header';
+import HomePage from './components/HomePage';
+import {BrowserRouter, Routes, Route} from "react-router-dom";
+import OrderForm from './components/OrderForm';
+import TShirtDesigner from './components/ShirtPreview';
+
+
+function App() {
+
+  return (
+    <div className='entire-page'>
+      <Header />
+      <div className='content'>
+        <BrowserRouter>
+          <Routes>
+            <Route path='/' element={<HomePage />}/>
+            <Route path='/about' element={<AboutPage />}/>
+            <Route path='/orders' element={<OrderForm />}/>
+            <Route path='/design' element={<TShirtDesigner />}/>
+          </Routes>
+        </BrowserRouter>
+      </div>
+        <Footer />
+    </div>
+  )
+}
+
+export default App;
