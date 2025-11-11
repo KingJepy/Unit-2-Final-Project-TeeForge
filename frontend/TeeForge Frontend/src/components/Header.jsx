3c475d822b85d86e6d13b216bb95a6a3e0f04af8
KingJepy <jmoslander15@gmail.com>
2025-11-10 21:22:00 -0600

    updated the header since it was using href still and also changed it to have the log out button when logged in.

--- frontend/TeeForge Frontend/src/components/Header.jsx
+++ frontend/TeeForge Frontend/src/components/Header.jsx
@@ -1,19 +1,41 @@
 import React from "react";
 import './Header.css';
+import { useAuth } from "../context/AuthContext";
+import { useNavigate, Link } from 'react-router-dom'
+
 
 function Header () {
-    return (
-        <header className="header">
-            <h1 className="nameInHeader">TeeForge</h1>
-            <nav className="headerLinks">
-                <a href="/">Home</a>
-                <a> | </a>
-                <a href="/login">Login</a>
-                <a> | </a>
-                <a href="/design">Design</a>
-            </nav>
-        </header>
-    );
-};
+
+    const { user, logout } = useAuth();
+    const navigate = useNavigate();
+
+    const handleLogout = () => {
+        logout();
+        navigate("/") // this will redirect to home page after a logout
+    }
+
+  return (
+    <header className="header">
+      <h1 className="nameInHeader">TeeForge</h1>
+      <nav className="headerLinks">
+        <Link to="/">Home</Link>
+        <span> | </span>
+        {user ? (
+        <Link
+            to="/"
+            className="header-link"
+            onClick={handleLogout}
+        >
+            Logout
+        </Link>
+        ) : (
+        <Link to="/login" className="header-link">Login</Link>
+        )}
+        <span> | </span>
+        <Link to="/design">Design</Link>
+      </nav>
+    </header>
+  );
+}
 
 export default Header;
